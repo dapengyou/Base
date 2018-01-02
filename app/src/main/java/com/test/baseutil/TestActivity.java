3bd9d0b2c90ee3a0505e26368806ac13612d266d
dapengyou <ladyztt@sina.com>
2018-01-02 14:37:52 +0800

    添加验证码倒计时工具类

--- app/src/main/java/com/test/baseutil/TestActivity.java
+++ app/src/main/java/com/test/baseutil/TestActivity.java
@@ -1,20 +1,20 @@
 package com.test.baseutil;
 
-import android.app.NotificationManager;
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
-import android.app.Activity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.test.baselibrary.Utils.CheckTextUtils;
+import com.test.baselibrary.Utils.CountDownUtil;
 import com.test.baselibrary.Utils.NotificationsUtils;
 import com.test.baselibrary.Utils.NumberUtils;
 
@@ -26,6 +26,8 @@ public class TestActivity extends Activity implements View.OnClickListener {
     private EditText mEtPassword;
     private EditText mEtPhone;
     private EditText mEtEmail;
+
+    private TextView mTvCountDown;
 
     private String mPhone;
     private String mPassword;
@@ -49,12 +51,15 @@ public class TestActivity extends Activity implements View.OnClickListener {
         mEtEmail = findViewById(R.id.et_email);
         mEtPassword = findViewById(R.id.et_password);
         mEtPhone = findViewById(R.id.et_phone);
+
+        mTvCountDown = findViewById(R.id.tv_countDown);
     }
 
     private void initListener() {
         mBtSetting.setOnClickListener(this);
         mBtNumber.setOnClickListener(this);
         mBtTest.setOnClickListener(this);
+        mTvCountDown.setOnClickListener(this);
     }
 
 
@@ -77,6 +82,13 @@ public class TestActivity extends Activity implements View.OnClickListener {
                 if (checkForm()) {  //验证手机号、邮箱地址、密码输入的是否正确
                     show("unbelievable");
                 }
+                break;
+            case R.id.tv_countDown:
+                show("点击了");
+//                CountDownUtil countDown = new CountDownUtil(mTvCountDown);
+                CountDownUtil countDown = new CountDownUtil(mTvCountDown,"%ds后重新发送");
+//                CountDownUtil countDown = new CountDownUtil(mTvCountDown,180,"%ds后重新发送");
+                countDown.start();
                 break;
 
         }
