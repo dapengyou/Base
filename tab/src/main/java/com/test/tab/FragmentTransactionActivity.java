220e0cbd17f95211c749a6a21a5b474b869dba9a
dapengyou <ladyztt@sina.com>
2018-02-28 16:08:59 +0800

    使用Butter Knife 框架注解方式跳转三个页面

---
+++ tab/src/main/java/com/test/tab/FragmentTransactionActivity.java
@@ -0,0 +1,13 @@
+package com.test.tab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragmentTransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_transaction);
+    }
+}
--- tab/src/main/java/com/test/tab/MainActivity.java
+++ tab/src/main/java/com/test/tab/MainActivity.java
@@ -1,13 +1,43 @@
 package com.test.tab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.bt_tab1)
+    Button wayOne;
+    @BindView(R.id.bt_tab2)
+    Button wayTwo;
+    @BindView(R.id.bt_tab3)
+    Button wayThree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.bt_tab1)
+    public void clickTab1() {
+        startActivity(new Intent(this,ViewpagerAndFragmentActivity.class));
+    }
+
+    @OnClick(R.id.bt_tab2)
+    public void clickTab2() {
+        startActivity(new Intent(this,FragmentTransactionActivity.class));
+
+    }
+
+    @OnClick(R.id.bt_tab3)
+    public void clickTab3() {
+        startActivity(new Intent(this,TablayoutActivity.class));
+
     }
 }
